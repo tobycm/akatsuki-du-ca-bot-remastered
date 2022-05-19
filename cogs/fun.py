0ce9fdc60ae75ae8dcee4b7d7195990c938a7f6b
Toby <newtobycm@gmail.com>
2022-05-18 19:43:51 -0700

    back to v0

--- cogs/fun.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-from discord import app_commands
-
-class fun(commands.Cog):
-
-    @app_commands.command(name="slap")
-    async def slap(self, interaction : discord.Interaction):
-        await interaction.response.send_message("slaped!")
