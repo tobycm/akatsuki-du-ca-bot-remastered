d53874d1742615eaf5adc64f9d0f6c8be8c8f623
Toby <newtobycm@gmail.com>
2022-06-25 15:06:41 -0700

    YESSIR

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
 import json
 
-bot = discord.Client(intents = discord.Intents.all())
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents = discord.Intents.all())
+tree = bot.tree
 
 with open("./config/settings.json", "r") as f:
     config = json.load(f)
+    
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    
+@bot.command(name = "sc", hidden = True)
+async def sc(ctx):
+    print(config["guild_id"])
+    await tree.sync(guild = discord.Object(id = config["guild_id"]))
+    await ctx.send("Synced!")
+
+    
+@tree.command(guild = discord.Object(id = config["guild_id"]))
+@app_commands.describe(text = "say what")
+async def say(interaction: discord.Interaction, text: str):
+    await interaction.channel.send(text)
     
 bot.run(config["discord_token"])
