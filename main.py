5c7f3163885ee39d54c5445d08a41908003d1242
Toby <newtobycm@gmail.com>
2022-06-26 16:25:06 -0700

    main.py init-ed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-from discord import Object, Interaction, app_commands, Intents, AllowedMentions
+from discord import Object, Interaction, app_commands, Intents, AllowedMentions, Game
 from discord.ext import commands
 
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ other modules import
@@ -8,12 +7,17 @@ from discord.ext import commands
 
 from modules.vault import get_token
 
-bot = commands.Bot(command_prefix=commands.when_mentioned, intents = Intents.all())
+bot = commands.Bot(command_prefix=commands.when_mentioned, activity=Game(name="Hibiki Ban Mai"), intents = Intents.all())
 tree = bot.tree
+bot.load_extension('jishaku')
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ bot settings
+# -----------------------------------------------------
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv sync command
     
 @bot.command(name = "sc", hidden = True)
 async def sc(ctx):
@@ -21,10 +25,21 @@ async def sc(ctx):
     await tree.sync(guild = Object(id = get_token("guild_id")))
     await ctx.send("Synced!")
 
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ sync command
+# -----------------------------------------------------
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv for events code
+
+@bot.event
+async def on_guild_join(guild):
+    print(f"Joined {guild.name}")
+    # guild_create(guild.id, guild.name, guild.owner.id)
     
-@tree.command(guild = Object(id = get_token("guild_id")))
-@app_commands.describe(text = "say what")
-async def say(interaction: Interaction, text: str):
-    await interaction.response.send_message(text, allowed_mentions = AllowedMentions.none())
+async def on_guild_remove(guild):
+    print(f"Left {guild.name}")
+    # guild_delete(guild.id)
     
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ for events code
+# -----------------------------------------------------
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv assembling bot
+
 bot.run(get_token("discord_token"))
