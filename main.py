08f4380abefc7861d3826d64370603d7b190a20f
tobycm <newtobycm@gmail.com>
2022-07-31 16:02:36 -0700

    use logging library + import prefix cog

--- main.py
+++ main.py
@@ -23,17 +23,18 @@ from cogs.music import RadioMusic
 from cogs.nsfw import NSFWCog
 from cogs.toys import ToysCog
 from cogs.utils import UtilsCog, MinecraftCog
+from cogs.admin import PrefixCog
 
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ cog import
 # -------------------------------------------------
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv bot settings
 
-default_prefix = "$"
+DEFAULT_PREFIX = "$"
 
 async def get_prefix_for_bot(bot, message):
     prefix = await get_prefix(bot.redis_ins, message.guild.id)
     if prefix is None:
-        return default_prefix
+        return DEFAULT_PREFIX
     return prefix
 
 bot = Bot(
@@ -43,11 +44,11 @@ bot = Bot(
          )
 tree = bot.tree
 
-logger = logging.getLogger('discord')
+bot_logger = logging.getLogger('discord')
 
 @bot.event
 async def on_ready():
-    logger.info(f"Logged in as {bot.user}")
+    bot_logger.info(f"Logged in as {bot.user}")
     
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ bot settings
 # -----------------------------------------------------
@@ -67,11 +68,11 @@ async def sc(ctx : Context):
 
 @bot.event
 async def on_guild_join(guild):
-    logger.info(f"Joined {guild.name}")
+    bot_logger.info(f"Joined {guild.name}")
     
 @bot.event
 async def on_guild_remove(guild):
-    logger.info(f"Left {guild.name}")
+    bot_logger.info(f"Left {guild.name}")
     
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ for events code
 # -----------------------------------------------------
@@ -84,20 +85,22 @@ async def start_up():
     bot.quotes_added = time()
     
     await bot.load_extension('jishaku')
-    logger.info("Loaded jishaku")
+    bot_logger.info("Loaded jishaku")
     
     await bot.add_cog(FunCog(bot))
     await bot.add_cog(GIFCog(bot))
-    logger.info("-> Fun and GIF Cog added <-")
+    bot_logger.info("-> Fun and GIF Cog added <-")
     await bot.add_cog(RadioMusic(bot))
-    logger.info(" -> Radio music cog added <-")
+    bot_logger.info(" -> Radio music cog added <-")
     await bot.add_cog(NSFWCog(bot))
-    logger.info(" -> NSFW cog added <-")
+    bot_logger.info(" -> NSFW cog added <-")
     await bot.add_cog(ToysCog(bot))
-    logger.info(" -> Toys cog added <-")
+    bot_logger.info(" -> Toys cog added <-")
     await bot.add_cog(UtilsCog(bot))
     await bot.add_cog(MinecraftCog(bot))
-    logger.info(" -> Utils and Minecraft cog added <-")
+    bot_logger.info(" -> Utils and Minecraft cog added <-")
+    await bot.add_cog(PrefixCog(bot))
+    bot_logger.info(" -> Prefix cog added <-")
     
 
 bot.setup_hook = start_up
