df98e219bf9abebb306a7a366ae109fc9929afa0
tobycm <newtobycm@gmail.com>
2022-08-02 10:18:52 -0700

    actually clean code for once 😂

--- cogs/music.py
+++ cogs/music.py
@@ -56,6 +56,42 @@ class MusicCog(Cog):
         """
         bot_logger = logging.getLogger('discord')
         bot_logger.info(f"Connected to {node.host}:{node.port}")
+        
+    async def _connect1(self, interaction : Interaction, lang : dict) -> wavelink.Player or None or str("error"):
+        if not interaction.user.voice: # author not in voice channel
+            await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
+            return "error"
+        if interaction.guild.voice_client:
+            if interaction.guild.voice_client.channel is not interaction.user.voice.channel:
+                await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
+                return "error"
+            elif interaction.guild.voice_client.channel is interaction.user.voice.channel:
+                await interaction.response.send_message(lang["music"]["voice_client"]["error"]["already_connected"])
+                return interaction.guild.voice_client
+        return None
+    
+    async def _connect2(self, interaction : Interaction, lang : dict) -> wavelink.Player:
+        await interaction.response.send_message(lang["music"]["voice_client"]["status"]["connecting"])
+        await interaction.user.voice.channel.connect(self_deaf = True, cls = wavelink.Player)
+        return await interaction.edit_original_message(
+            content = lang["music"]["voice_client"]["status"]["connected"]
+        )
+        
+    async def _disconnect1(self, interaction : Interaction, lang : dict) -> None or True:
+        if not interaction.user.voice: # author not in voice channel
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
+        if not interaction.guild.voice_client: # bot didn't even connect lol
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["not_connected"])
+        if interaction.guild.voice_client.channel != interaction.user.voice.channel:
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
+        return True
+    
+    async def _disconnect2(self, interaction : Interaction, lang : dict) -> None or True:
+        await interaction.response.send_message(lang["music"]["voice_client"]["status"]["disconnecting"])
+        await interaction.guild.voice_client.disconnect()
+        return await interaction.edit_original_message(
+            content = lang["music"]["voice_client"]["status"]["disconnected"]
+        )
 
     @app_commands.checks.cooldown(1, 1.5, key = user_cooldown_check)
     @app_commands.command(name = "connect")
@@ -67,19 +103,10 @@ class MusicCog(Cog):
         command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
         lang = await return_user_lang(self, author.id)
         
-        if not interaction.user.voice: # author not in voice channel
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
-        if interaction.guild.voice_client:
-            if interaction.guild.voice_client.channel is not interaction.user.voice.channel:
-                return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
-            elif interaction.guild.voice_client.channel is interaction.user.voice.channel:
-                return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["already_connected"])
-
-        await interaction.response.send_message(lang["music"]["voice_client"]["status"]["connecting"])
-        await interaction.user.voice.channel.connect(self_deaf = True, cls = wavelink.Player)
-        return await interaction.edit_original_message(
-            content = lang["music"]["voice_client"]["status"]["connected"]
-        )
+        player = await self._connect1(interaction, lang)
+        if player is None:
+            player = await self._connect2(interaction, lang)
+        return
         
     @app_commands.checks.cooldown(1, 1.5, key = user_cooldown_check)
     @app_commands.command(name = "disconnect")
@@ -91,18 +118,10 @@ class MusicCog(Cog):
         command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
         lang = await return_user_lang(self, author.id)
         
-        if not interaction.user.voice: # author not in voice channel
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
-        if not interaction.guild.voice_client: # bot didn't even connect lol
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["not_connected"])
-        if interaction.guild.voice_client.channel != interaction.user.voice.channel:
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
-        
-        await interaction.response.send_message(lang["music"]["voice_client"]["status"]["disconnecting"])
-        await interaction.guild.voice_client.disconnect()
-        return await interaction.edit_original_message(
-            content = lang["music"]["voice_client"]["status"]["disconnected"]
-        )
+        result = await self._disconnect1(interaction, lang)
+        if result:
+            await self._disconnect2(interaction, lang)
+        return
     
     @app_commands.checks.cooldown(1, 1.25, key = user_cooldown_check)
     @app_commands.command(name = "play")
@@ -114,28 +133,15 @@ class MusicCog(Cog):
         command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
         lang = await return_user_lang(self, author.id)
         
-        # duplicate connect
-        if not interaction.user.voice: # author not in voice channel
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
-        if interaction.guild.voice_client:
-            if interaction.guild.voice_client.channel is not interaction.user.voice.channel:
-                return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
-            vcl : wavelink.Player = interaction.guild.voice_client
-            bot_already_in_voice = True
-
-        if not interaction.guild.voice_client: # bot didn't even connect lol
-            await interaction.response.send_message(lang["music"]["voice_client"]["status"]["connecting"])
-            vcl : wavelink.Player = await interaction.user.voice.channel.connect(self_deaf = True, cls = wavelink.Player)
-            await interaction.edit_original_message(
-                content = lang["music"]["voice_client"]["status"]["connected"]
-            )
+        player = await self._connect1(interaction, lang)
         
+        if player is not None:
+            return
+        
+        player : wavelink.Player = await self._connect2(interaction, lang)
         track : wavelink.YouTubeTrack = await wavelink.YouTubeTrack.search(query = query, return_first=True)
 
-        await vcl.play(track)
-                    
-        if bot_already_in_voice:
-            return await interaction.response.send_message(lang["music"]["voice_client"]["status"]["playing"])
+        await player.play(track)
         return await interaction.channel.send(lang["music"]["voice_client"]["status"]["playing"])
     
     @app_commands.checks.cooldown(1, 1.25, key = user_cooldown_check)
