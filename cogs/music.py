a435c4bbc89d46cd9c26489df4a55d0a275c5217
tobycm <newtobycm@gmail.com>
2022-08-01 22:01:52 -0700

    added bunch new commands to music cog

--- cogs/music.py
+++ cogs/music.py
@@ -69,13 +69,99 @@ class MusicCog(Cog):
         
         if not interaction.user.voice: # author not in voice channel
             return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
-        if interaction.guild.voice_client.channel is interaction.user.voice.channel: # bot is already connected lmao
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["already_connected"])
-        elif interaction.guild.voice_client: # in another voice channel
-            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
-        
+        if interaction.guild.voice_client:
+            if interaction.guild.voice_client.channel is not interaction.user.voice.channel:
+                return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
+            elif interaction.guild.voice_client.channel is interaction.user.voice.channel:
+                return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["already_connected"])
+
         await interaction.response.send_message(lang["music"]["voice_client"]["status"]["connecting"])
-        await interaction.user.voice.channel.connect() 
+        await interaction.user.voice.channel.connect(self_deaf = True, cls = wavelink.Player)
         return await interaction.edit_original_message(
             content = lang["music"]["voice_client"]["status"]["connected"]
         )
+        
+    @app_commands.checks.cooldown(1, 1.5, key = user_cooldown_check)
+    @app_commands.command(name = "disconnect")
+    async def disconnect(self, interaction : Interaction):
+        """
+        Disconnect from a voice channel.
+        """
+        author = interaction.user
+        command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
+        lang = await return_user_lang(self, author.id)
+        
+        if not interaction.user.voice: # author not in voice channel
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
+        if not interaction.guild.voice_client: # bot didn't even connect lol
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["not_connected"])
+        if interaction.guild.voice_client.channel != interaction.user.voice.channel:
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
+        
+        await interaction.response.send_message(lang["music"]["voice_client"]["status"]["disconnecting"])
+        await interaction.guild.voice_client.disconnect()
+        return await interaction.edit_original_message(
+            content = lang["music"]["voice_client"]["status"]["disconnected"]
+        )
+    
+    @app_commands.checks.cooldown(1, 1.25, key = user_cooldown_check)
+    @app_commands.command(name = "play")
+    async def play(self, interaction : Interaction, query : str):
+        """
+        Play a song.
+        """
+        author = interaction.user
+        command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
+        lang = await return_user_lang(self, author.id)
+        
+        # duplicate connect
+        if not interaction.user.voice: # author not in voice channel
+            return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["user_no_voice"])
+        if interaction.guild.voice_client:
+            if interaction.guild.voice_client.channel is not interaction.user.voice.channel:
+                return await interaction.response.send_message(lang["music"]["voice_client"]["error"]["playing_in_another_channel"])
+            vcl : wavelink.Player = interaction.guild.voice_client
+            bot_already_in_voice = True
+
+        if not interaction.guild.voice_client: # bot didn't even connect lol
+            await interaction.response.send_message(lang["music"]["voice_client"]["status"]["connecting"])
+            vcl : wavelink.Player = await interaction.user.voice.channel.connect(self_deaf = True, cls = wavelink.Player)
+            await interaction.edit_original_message(
+                content = lang["music"]["voice_client"]["status"]["connected"]
+            )
+        
+        track : wavelink.YouTubeTrack = await wavelink.YouTubeTrack.search(query = query, return_first=True)
+
+        await vcl.play(track)
+                    
+        if bot_already_in_voice:
+            return await interaction.response.send_message(lang["music"]["voice_client"]["status"]["playing"])
+        return await interaction.channel.send(lang["music"]["voice_client"]["status"]["playing"])
+    
+    @app_commands.checks.cooldown(1, 1.25, key = user_cooldown_check)
+    @app_commands.command(name = "pause")
+    async def pause(self, interaction : Interaction):
+        """
+        Pause a song.
+        """
+        author = interaction.user
+        command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
+        lang = await return_user_lang(self, author.id)
+        
+        vcl : wavelink.Player = interaction.guild.voice_client
+        await vcl.pause()
+        return await interaction.response.send_message(lang["music"]["voice_client"]["status"]["paused"])
+    
+    @app_commands.checks.cooldown(1, 1.25, key = user_cooldown_check)
+    @app_commands.command(name = "resume")
+    async def resume(self, interaction : Interaction):
+        """
+        Resume a song.
+        """
+        author = interaction.user
+        command_log(author.id, author.guild.id, interaction.channel.id, interaction.command.name)
+        lang = await return_user_lang(self, author.id)
+        
+        vcl : wavelink.Player = interaction.guild.voice_client
+        await vcl.resume()
+        return await interaction.response.send_message(lang["music"]["voice_client"]["status"]["resumed"])
