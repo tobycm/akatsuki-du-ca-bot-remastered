2b54f7d2582c1850e5bb7c593917607cddd5b4a8
tobycm <newtobycm@gmail.com>
2022-09-02 11:16:49 -0700

    hotfix waifu api

--- modules/waifu_api.py
+++ modules/waifu_api.py
@@ -25,7 +25,7 @@ async def get_waifu_image_url() -> tuple(("url", "source")):
     url = f"https://api.waifu.im/random/?selected_tags={cate}"
     async with ClientSession() as session:
         async with session.get(url) as response:
-            data = response.json()
+            data = await response.json()
 
     if data["images"]:
         url = data["images"][0]["url"]
